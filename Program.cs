8928fcdcd5afca42567db2ac4ae9e70a92f7c5b8
maksimnelubin1-bit <maksimnelubin1@gmail.com>
2026-06-21 23:24:18 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using FitnessCenter.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<FitnessCenterDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+// Configure pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
